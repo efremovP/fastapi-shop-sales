eb43db4139ae3ddb9cc162ec46c11c07211b39e3
HOME-PC\Петр <peter.efremov@gmail.com>
2021-09-26 19:47:32 +0500

    add report

--- src/sales/operations/report_service.py
+++ src/sales/operations/report_service.py
@@ -1,9 +1,11 @@
 from typing import List
 from datetime import date
+from .models import TypeOperationEnum
 
-config = {
+report_config = {
 	'empty_category_name': 'Без категории',
-	'amount_accuracy': 2,
+	'buy': 'Покупки',
+	'sale': 'Продажи',
 }
 
 class Report():
@@ -11,75 +13,91 @@ class Report():
 
 	def __init__(self):
 		self.header_column_list = []
-		self.report_data = {}
+		self._report_data = {}
 
 	def get_result(self) -> tuple:
-		result = {"time_points": self.header_column_list} | self.report_data
-		    # "buy": self.report_data[TypeOperationEnum.buy],
-		    # "sale": self.report_data[TypeOperationEnum.sale]}
+		view_report_data = self._make_view_format({})
+		result = {"time_points": self.header_column_list} | view_report_data
+
 		return result
 
+	def _make_view_format(self, view_report_data):
+		for type in self._report_data:
+			self._report_data[type]['amounts'] = list(self._report_data[type]['amounts'].values())
+			self._report_data[type]['children'] = self._iter_result_dict(self._report_data[type]['children'])
+			view_report_data = view_report_data | {type: self._report_data[type]}
+
+		return view_report_data
+
+	def _iter_result_dict(self, values):
+		res_values = list()
+		for value in values:
+			local_values = values[value]
+			local_values['amounts'] = list(local_values['amounts'].values())
+			if 'children' in local_values:
+				local_values['children'] = self._iter_result_dict(local_values['children'])
+
+			res_values.append(local_values)
+
+		return res_values
+
+
+
 	def add_row(self, row: dict) -> None:
-		if row['category'] == '':
-			row['category'] = config['empty_category_name']
-			# row_date = date.fromisoformat(str(row['date'])).replace(day=1)
 
-		self._add_new_element(row['type'], row['shop'], row['category'], row['name'], row['date'])
+		category = row['category'] or report_config['empty_category_name']
 
-		product_revenue = self._get_product_revenue(row['amount'], row['price'])
+		row_date = date.fromisoformat(str(row['date'])).replace(day=1)
 
-		self._add_revenue_to_element(row['type'], row['shop'], row['category'], row['name'], row['date'], product_revenue)
+		self._add_new_element(row['type'], row['shop'], category, row['name'], row_date)
 
-	def _add_new_element(self, type: str, shop: str, category: str, name: str,
-						 column_date: str) -> None:
+		self._add_revenue_to_element(row['type'], row['shop'], category, row['name'], row_date, row['amount'])
 
-		if not type in self.report_data:
-			self.report_data[type] = {'total': 0, 'revenue':{}, 'children': {}}
+	def _add_new_element(self, type: str, shop: str, category: str, name: str,  column_date: str) -> None:
 
-		if not shop in self.report_data[type]['children']:
-			self.report_data[type]['children'][shop] =  {'total': 0, 'revenue':{}, 'children': {}}
+		if not type in self._report_data:
+			self._report_data[type] = {'name': report_config[type.value], 'amounts': {}, 'total_amount': 0, 'children': {}}
 
-		if not category in self.report_data[type]['children'][shop]['children']:
-			self.report_data[type]['children'][shop]['children'][category] = {'total': 0, 'revenue':{}, 'children': {}}
+		if not shop in self._report_data[type]['children']:
+			self._report_data[type]['children'][shop] = {'name': shop, 'amounts': {}, 'total_amount': 0, 'children': {}}
 
-		if not name in self.report_data[type]['children'][shop]['children'][category]['children']:
-			self.report_data[type]['children'][shop]['children'][category]['children'][name] = {'total': 0, 'revenue':{}}
+		if not category in self._report_data[type]['children'][shop]['children']:
+			self._report_data[type]['children'][shop]['children'][category] = {'name': category, 'amounts': {}, 'total_amount': 0, 'children': {}}
 
-		if not column_date in self.report_data[type]['revenue']:
-			self.report_data[type]['revenue'][column_date] = 0
+		if not name in self._report_data[type]['children'][shop]['children'][category]['children']:
+			self._report_data[type]['children'][shop]['children'][category]['children'][name] = {'name': name, 'amounts': {}, 'total_amount': 0}
 
-		if not column_date in self.report_data[type]['children'][shop]['revenue']:
-			self.report_data[type]['children'][shop]['revenue'][column_date] = 0
+		if not column_date in self._report_data[type]['amounts']:
+			self._report_data[type]['amounts'][column_date] = 0
 
-		if not column_date in self.report_data[type]['children'][shop]['children'][category]['revenue']:
-			self.report_data[type]['children'][shop]['children'][category]['revenue'][column_date] = 0
+		if not column_date in self._report_data[type]['children'][shop]['amounts']:
+			self._report_data[type]['children'][shop]['amounts'][column_date] = 0
 
-		if not column_date in self.report_data[type]['children'][shop]['children'][category]['children'][name]['revenue']:
-			self.report_data[type]['children'][shop]['children'][category]['children'][name]['revenue'][column_date] = 0
+		if not column_date in self._report_data[type]['children'][shop]['children'][category]['amounts']:
+			self._report_data[type]['children'][shop]['children'][category]['amounts'][column_date] = 0
 
-	def _add_revenue_to_element(self, type: str, shop: str, category: str, name: str,
-								column_date: str, product_revenue: float) -> None:
+		if not column_date in self._report_data[type]['children'][shop]['children'][category]['children'][name]['amounts']:
+			self._report_data[type]['children'][shop]['children'][category]['children'][name]['amounts'][column_date] = 0
 
-		self.report_data[type]['total'] += product_revenue
-		self.report_data[type]['revenue'][column_date] += product_revenue
+	def _add_revenue_to_element(self, type: str, shop: str, category: str, name: str, column_date: str, amount: float) -> None:
+
+		self._report_data[type]['total_amount'] += amount
+		self._report_data[type]['amounts'][column_date] += amount
 
 		self._add_date_to_header_column_list(column_date)
 
-		self.report_data[type]['children'][shop]['total'] += product_revenue
-		self.report_data[type]['children'][shop]['revenue'][column_date] += product_revenue
+		self._report_data[type]['children'][shop]['total_amount'] += amount
+		self._report_data[type]['children'][shop]['amounts'][column_date] += amount
 
-		self.report_data[type]['children'][shop]['children'][category]['total'] += product_revenue
-		self.report_data[type]['children'][shop]['children'][category]['revenue'][column_date] += product_revenue
+		self._report_data[type]['children'][shop]['children'][category]['total_amount'] += amount
+		self._report_data[type]['children'][shop]['children'][category]['amounts'][column_date] += amount
 
-		self.report_data[type]['children'][shop]['children'][category]['children'][name]['total'] += product_revenue
-		self.report_data[type]['children'][shop]['children'][category]['children'][name]['revenue'][column_date] += product_revenue
+		self._report_data[type]['children'][shop]['children'][category]['children'][name]['total_amount'] += amount
+		self._report_data[type]['children'][shop]['children'][category]['children'][name]['amounts'][column_date] += amount
 
 	def _add_date_to_header_column_list(self, column_date: str) -> None:
 		if not column_date in self.header_column_list:
 			self.header_column_list.append(column_date)
-
-	def _get_product_revenue(self, amount: str, price: str) -> float:
-		return int(amount) * float(price)
 
 
 def main():
